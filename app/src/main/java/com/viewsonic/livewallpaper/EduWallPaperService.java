8f9835e9a3e8e610ddbe9cca52d32b35166176f1
Andy Lai <andy.lai@tw.viewsonic.com>
2019-11-04 15:14:06 +0800

    [Fix] add multi thread process

--- app/src/main/java/com/viewsonic/livewallpaper/EduWallPaperService.java
+++ app/src/main/java/com/viewsonic/livewallpaper/EduWallPaperService.java
@@ -10,11 +10,12 @@ import android.graphics.PorterDuff;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.service.wallpaper.WallpaperService;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 public class EduWallPaperService extends WallpaperService {
 
-	private final byte[] mLock = new byte[0];
+	private final String TAG = getClass().getSimpleName();
 
 	@Override
 	public Engine onCreateEngine() {
@@ -27,17 +28,7 @@ public class EduWallPaperService extends WallpaperService {
 		private int mWidth;
 		private int mHeight;
 		private boolean mVisible = true;
-		private	Bitmap mBitmapBG;
-
-
-		private HandlerThread thread = new HandlerThread("WallPaperService");
-		private Handler handler;
-		private final Runnable drawRunner = new Runnable() {
-			@Override
-			public void run() {
-				draw();
-			}
-		};
+		private Bitmap mBitmapBG;
 
 		private float mDegree;
 		// Used for rotation at right top of screen with a rounded rectangle
@@ -49,11 +40,20 @@ public class EduWallPaperService extends WallpaperService {
 		private int[] mRotationPivot1 = {1800, -500};
 		private int mCornerRadius = 408;
 		private int mSideLength = 2044;
-		private int mHalfSideLength = mSideLength /2;
+		private int mHalfSideLength = mSideLength / 2;
 		// Config for circle
 		private int[] mRotationPivot2 = {450, 1560};
 		private int[] mOffset = {-50, 60};
 		private int mRadius = 1091;
+
+		private HandlerThread thread = new HandlerThread("WallPaperService");
+		private Handler handler;
+		private final Runnable drawRunner = new Runnable() {
+			@Override
+			public void run() {
+				draw();
+			}
+		};
 
 		public EduEngine() {
 			mBitmapBG = BitmapFactory.decodeResource(getResources(), R.mipmap.gra_bg_gradual);
@@ -87,54 +87,82 @@ public class EduWallPaperService extends WallpaperService {
 		@Override
 		public void onSurfaceChanged(SurfaceHolder holder, int format,
 				int width, int height) {
-			mWidth = width;
-			mHeight = height;
 			super.onSurfaceChanged(holder, format, width, height);
+			determineShapeSize(width, height);
 		}
 
-		private void draw() {
-			synchronized (mLock) {
-				calculateRotationDegree();
-				SurfaceHolder holder = getSurfaceHolder();
-				Canvas canvas = null;
-				paint.setColor(Color.WHITE);
-				paint.setAntiAlias(true);
-				paint.setFilterBitmap(true);
-				paint.setAlpha(30);
+		private void determineShapeSize(int width, int height) {
+			if (width > 0 && height > 0) {
+				// Config for rounded rectangle
+				mRotationPivot1[0] = (int) (0.9375f * width);
+				mRotationPivot1[1] = (int) (-0.5 * height);
+				mCornerRadius = (int) (0.21 * width);
+				mSideLength = (int) (1.06 * width);
+				mHalfSideLength = mSideLength / 2;
+				// Config for circle
+				mRotationPivot2[0] = (int) (0.23 * width);
+				mRotationPivot2[1] = (int) (0.81 * width);
+				mOffset[0] = (int) (-0.03 * width);
+				mOffset[1] = (int) (0.03 * width);
+				mRadius = (int) (0.57 * width);
 
-				try {
-					canvas = holder.lockCanvas();
-					PaintFlagsDrawFilter filter = new PaintFlagsDrawFilter(0, Paint.FILTER_BITMAP_FLAG);
-					canvas.setDrawFilter(filter);
-					// clear canvas
-					canvas.drawColor(0, PorterDuff.Mode.CLEAR);
-					// draw original mBitmapBG
-					canvas.drawBitmap(mBitmapBG, 0, 0, null);
-					// draw right top rounded rectangle
-					canvas.save();
-					canvas.rotate(mDegree1, mRotationPivot1[0], mRotationPivot1[1]);
-					canvas.drawRoundRect(
-							mRotationPivot1[0] - mHalfSideLength,
-							mRotationPivot1[1] - mHalfSideLength,
-							mRotationPivot1[0] + mHalfSideLength,
-							mRotationPivot1[1] + mHalfSideLength,
-							mCornerRadius, mCornerRadius, paint);
-					canvas.restore();
-					// draw left bottom circle
-					canvas.save();
-					canvas.rotate(mDegree2, mRotationPivot2[0], mRotationPivot2[1]);
-					canvas.drawCircle(mRotationPivot2[0]+ mOffset[0], mRotationPivot2[1] + mOffset[1], mRadius, paint);
-					canvas.restore();
-				} catch (Exception e) {
-					e.printStackTrace();
-				} finally {
-					if (canvas != null)
-						holder.unlockCanvasAndPost(canvas);
-				}
-
-				handler.removeCallbacks(drawRunner);
-				handler.postDelayed(drawRunner, 30);
+				Log.d(TAG, "\n" +
+						"mRotationPivot1 = " + mRotationPivot1[0] + ", " + mRotationPivot1[1] + "\n" +
+						"mCornerRadius = " + mCornerRadius + "\n" +
+						"mSideLength = " + mSideLength + "\n" +
+						"mHalfSideLength = " + mHalfSideLength + "\n" +
+						"mRotationPivot2 = " + mRotationPivot2[0] + ", " + mRotationPivot2[1] + "\n" +
+						"mOffset = " + mOffset[0] + ", " + mOffset[1] + "\n" +
+						"mRadius = " + mRadius);
 			}
+		}
+
+		private synchronized void draw() {
+			calculateRotationDegree();
+			SurfaceHolder holder = getSurfaceHolder();
+			Canvas canvas = null;
+			paint.setColor(Color.WHITE);
+			paint.setAntiAlias(true);
+			paint.setFilterBitmap(true);
+			paint.setAlpha(30);
+
+			try {
+				canvas = holder.lockCanvas();
+				PaintFlagsDrawFilter filter = new PaintFlagsDrawFilter(0, Paint.FILTER_BITMAP_FLAG);
+				canvas.setDrawFilter(filter);
+				// clear canvas
+				canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+				// draw original mBitmapBG
+				canvas.drawBitmap(mBitmapBG, 0, 0, null);
+				// draw right top rounded rectangle
+				canvas.save();
+				canvas.rotate(mDegree1, mRotationPivot1[0], mRotationPivot1[1]);
+				canvas.drawRoundRect(
+						mRotationPivot1[0] - mHalfSideLength,
+						mRotationPivot1[1] - mHalfSideLength,
+						mRotationPivot1[0] + mHalfSideLength,
+						mRotationPivot1[1] + mHalfSideLength,
+						mCornerRadius, mCornerRadius, paint);
+				canvas.restore();
+				// draw left bottom circle
+				canvas.save();
+				canvas.rotate(mDegree2, mRotationPivot2[0], mRotationPivot2[1]);
+				canvas.drawCircle(mRotationPivot2[0] + mOffset[0], mRotationPivot2[1] + mOffset[1], mRadius, paint);
+				canvas.restore();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (canvas != null && holder.getSurface() != null) {
+						holder.unlockCanvasAndPost(canvas);
+					}
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				}
+			}
+
+			handler.removeCallbacks(drawRunner);
+			handler.postDelayed(drawRunner, 30);
 		}
 
 		private void calculateRotationDegree() {
--- app/src/main/java/com/viewsonic/livewallpaper/MainActivity.java
+++ app/src/main/java/com/viewsonic/livewallpaper/MainActivity.java
@@ -1,20 +1,9 @@
 package com.viewsonic.livewallpaper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.pm.ConfigurationInfo;
-import android.opengl.GLES20;
-import android.opengl.GLSurfaceView;
-import android.opengl.Matrix;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
-import android.view.SurfaceHolder;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
